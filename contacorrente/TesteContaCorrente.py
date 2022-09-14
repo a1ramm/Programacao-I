3c180cee6fa2b5ca854ef051fba004265427feb1
Maria <105741044+a1ramm@users.noreply.github.com>
2022-09-14 14:17:04 -0300

    classe TesteContaCorrente

--- contacorrente/TesteContaCorrente.py
+++ contacorrente/TesteContaCorrente.py
@@ -18,22 +18,38 @@ while True:
 print('')
 numconta = input("Digite numero da conta: ")
 
+conta_existe = False
 for conta in lista:
     if conta.numeroConta == numconta:
-        print(conta.exibirDados())
-        operação = int(input("1 - para SACAR e 2 - para DEPOSITAR: "))
+        conta_existe = True
 
-        if operação == 1:
-            sacar = float(input("Quanto deseja sacar: "))
-            print(conta.sacar(sacar))
-        if operação == 2:
-            depositar = float(input("Quanto deseja depositar: "))
-            print(conta.depositar(depositar))
-
-if len(lista) > 0:
-    print("Saldo de todas as contas")
+if conta_existe:
     for conta in lista:
-        print(conta.saldo)
+        if conta.numeroConta == numconta:
+            print(conta.exibirDados())
+            operação = int(input("1 - para SACAR e 2 - para DEPOSITAR: "))
+
+            if operação == 1:
+                valorSaque = float(input("Quanto deseja sacar: ")) 
+                if conta.sacar(valorSaque) == True:
+                    print(f"Saque realizado com sucesso. Saldo atual {conta.saldo}")
+                else:
+                    print(f"Saque não realizado. Saldo atual {conta.saldo}")
+            if operação == 2:
+                valorDeposito = float(input("Quanto deseja depositar: "))
+                if conta.depositar(valorDeposito) == True:
+                    print(f"Depósito realizado com sucesso. Saldo atual {conta.saldo}")
+                else:
+                    print(f"Depósito não realizado. Saldo atual {conta.saldo}")
+else:
+    print("conta n existe")
+
+print("")
+saldodetodos = 0
+if len(lista) > 0:
+    for conta in lista:
+        saldodetodos += conta.saldo
+    print(f"Saldo de todas as contas: {saldodetodos}")
 
     maior = -1
     for conta in lista:
@@ -46,5 +62,3 @@ if len(lista) > 0:
         if conta.saldo < menor:
             menor = conta.saldo
     print(f"Menor saldo: {menor}")
-
-
